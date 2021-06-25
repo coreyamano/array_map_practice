be95dd3338022b0b3332ffef29191254fe4194b1
Corey <corey.amano@gmail.com>
2021-06-25 12:15:14 -0700

    solve problem 10

--- array_map_practice.rb
+++ array_map_practice.rb
@@ -109,3 +109,12 @@ p double_ages
 
 # 10. Start with an array of numbers and create a new array with each number converted into a string.
 #     For example, [1, 2, 3] becomes ["1", "2", "3"].
+
+ints = [1, 2, 3]
+string_nums = []
+
+ints.each do |int|
+  string_nums << int.to_s
+end
+
+p string_nums
