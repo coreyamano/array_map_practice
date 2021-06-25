68170ab2e6f7f89a90ef7e2021f8d7f1fb8cf8ae
Corey <corey.amano@gmail.com>
2021-06-25 12:07:53 -0700

    solve problem 6

--- array_map_practice.rb
+++ array_map_practice.rb
@@ -62,6 +62,15 @@ p alien_lengths
 #  6. Start with an array of hashes and create a new array of number values from each hash's :age key.
 #     For example, [{name: "Alice", age: 27}, {name: "Blane", age: 16}] becomes [27, 16].
 
+watermelons = [{name: "Alice", age: 27}, {name: "Blane", age: 16}]
+ages = []
+
+watermelons.each do |watermelon|
+  ages << watermelon[:age]
+end
+
+p ages
+
 #  7. Start with an array of numbers and create a new array with each number divided by 2.
 #     For example, [1, 2, 3] becomes [0.5, 1.0, 1.5].
 
