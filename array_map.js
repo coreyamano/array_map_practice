9e816d0e78670c1d7df6efd2fb04f14b9c805e7f
Corey <corey.amano@gmail.com>
2021-07-17 22:29:43 -0700

    add solutions for problems 4-10

--- array_map.js
+++ array_map.js
@@ -57,18 +57,62 @@ console.log(lengths);
 // 6. Start with an array of hashes and create a new array of number values from each hash's :age key.
 //    For example, [{name: "Alice", age: 27}, {name: "Blane", age: 16}] becomes [27, 16].
 
+var people = [
+  { name: "Alice", age: 27 },
+  { name: "Blane", age: 16 },
+];
+var ages = [];
+
+people.forEach (function(person) {
+  ages.push(person.age);
+});
+
+console.log(ages);
 
 // 7. Start with an array of numbers and create a new array with each number divided by 2.
 //    For example, [1, 2, 3] becomes [0.5, 1.0, 1.5].
 
+var numbers = [1,2,3];
+var splitInTwos = [];
+
+numbers.forEach(function(number) {
+  splitInTwos.push(number / 2.0);
+});
+
+console.log(splitInTwos);
 
 // 8. Start with an array of strings and create a new array with each string's first letter only.
 //    For example, ["hello", "goodbye"] becomes ["h", "g"].
 
+var greetings = ["hello", "goodbye"];
+var firstLetters = [];
+
+greetings.forEach(function(greeting) {
+  firstLetters.push(greeting[0]);
+});
+
+console.log(firstLetters);
 
 // 9.  Start with an array of hashes and create a new array of number values from each hash's :age key times 2.
 //    For example, [{name: "Alice", age: 27}, {name: "Blane", age: 16}] becomes [54, 32].
 
+var students = [{name: "Alice", age: 27}, {name: "Blane", age: 16}];
+var doubleAges = [];
+
+students.forEach(function(student) {
+  doubleAges.push(student.age * 2);
+});
+
+console.log(doubleAges);
 
 // 10. Start with an array of numbers and create a new array with each number converted into a string.
 //    For example, [1, 2, 3] becomes ["1", "2", "3"].
+
+var nums = [1, 2, 3];
+var stringNums = [];
+
+nums.forEach(function(num) {
+  stringNums.push(num.toString());
+});
+
+console.log(stringNums);
